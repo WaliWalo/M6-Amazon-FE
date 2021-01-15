59650741bd6dbfbba5d3926dded2f82865f73087
Hung Jin Chong <hungjinchong@Outlook.com>
2021-01-15 09:58:51 +0000

    cloned prev fe

---
+++ src/api/productsApi.js
@@ -0,0 +1,305 @@
+//GET ALL PRODUCTS
+export async function getProducts() {
+  try {
+    const response = await fetch(`http://localhost:3001/products`, {
+      method: "GET",
+    });
+    if (response.ok) {
+      let data = response.json();
+      return data;
+    } else {
+      let error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//POST A PRODUCT
+export async function postProduct(product) {
+  try {
+    const response = await fetch(`http://localhost:3001/products/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(product),
+    });
+    if (response.ok) {
+      alert("success");
+      let result = response.json();
+      return result;
+    } else {
+      alert("fuck");
+      let error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//GET A SINGLE PRODUCT
+export async function getSingleProduct(id) {
+  try {
+    const response = await fetch(`http://localhost:3001/products/${id}`, {
+      method: "GET",
+    });
+    if (response.ok) {
+      let data = response.json();
+      return data;
+    } else {
+      let error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//UPDATE A PRODUCT
+export async function updateProduct(id, product) {
+  try {
+    const response = await fetch(`http://localhost:3001/products/${id}`, {
+      method: "PUT",
+      header: new Headers("Content-Type", "application/json"),
+      body: JSON.stringify(product),
+    });
+    if (response.ok) {
+      let data = response.json();
+      return data;
+    } else {
+      let error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//REMOVE A PRODUCT
+export async function removeProduct(id) {
+  try {
+    const response = await fetch(`http://localhost:3001/products/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      return "Product Deleted";
+    } else {
+      return response.json();
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+// "/:id/reviews" GET ALL REVIEWS FOR A SINGLE PRODUCT
+export async function getAllReviews(productId) {
+  try {
+    const response = await fetch(
+      `http://localhost:3001/products/${productId}/reviews`,
+      { method: "GET" }
+    );
+    if (response.ok) {
+      let data = response.json();
+      return data;
+    } else {
+      let error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+// "/:id/reviews" POST A REVIEW FOR A PRODUCT
+export async function postReview(productId, review) {
+  try {
+    const response = await fetch(
+      `http://localhost:3001/products/${productId}/reviews`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(review),
+      }
+    );
+    if (response.ok) {
+      alert("successfuly added");
+      let result = response.json();
+      return result;
+    } else {
+      alert("fuck! smthing wrong");
+      let error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+// UPDATE A REVIEW
+export async function updateReview(reviewId, review) {
+  try {
+    const response = await fetch(
+      `http://localhost:3001/products/${reviewId}/reviews`,
+      {
+        method: "PUT",
+        headers: new Headers("Content-Type", "application/json"),
+        body: JSON.stringify(review),
+      }
+    );
+    if (response.ok) {
+      let result = response.json();
+      return result;
+    } else {
+      let error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//DELETE A REVIEW
+export async function deleteReview(reviewId) {
+  try {
+    const response = await fetch(
+      `http://localhost:3001/products/${reviewId}/reviews`,
+      { method: "DELETE" }
+    );
+    if (response.ok) {
+      return "REVIEW DELETED";
+    } else {
+      return response.json();
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//POST A IMAGE
+export async function postProductImage(productId, file) {
+  try {
+    console.log(file);
+    let formData = new FormData();
+    formData.append("product", file, file.name);
+    const response = await fetch(
+      `http://localhost:3001/products/${productId}/image`,
+      { method: "POST", body: formData }
+    );
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      const error = await response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//POST product to cart
+export async function addProductToCart(productId, cartId) {
+  try {
+    cartId = "5f6b1991df85440017160811";
+    let response = await fetch(
+      `http://localhost:3001/products/carts/${cartId}/addToCart/${productId}`,
+      { method: "POST" }
+    );
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      const error = await response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//GET BASKET
+export async function getBasket(cartId) {
+  try {
+    cartId = "5f6b1991df85440017160811";
+    let response = await fetch(
+      `http://localhost:3001/products/carts/${cartId}/`,
+      { method: "GET" }
+    );
+    if (response.ok) {
+      const data = await response.json();
+
+      return data;
+    } else {
+      const error = await response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//DELETE ITEM FROM BASKET
+export async function removeItemFromBasket(cartId, productId) {
+  try {
+    cartId = "5f6b1991df85440017160811";
+    let response = await fetch(
+      `http://localhost:3001/products/carts/${cartId}/removeFromCart/${productId}`,
+      { method: "DELETE" }
+    );
+    if (response.ok) {
+      const data = await response.json();
+
+      return data;
+    } else {
+      const error = await response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//GET PRODUCT PDF
+export async function getProductPdf(id) {
+  try {
+    let response = await fetch(
+      `http://localhost:3001/products/${id}/exportPdf`,
+      { method: "GET" }
+    );
+    const file = { file: `http://localhost:3001/products/${id}/exportPdf` };
+
+    if (response.ok) {
+      alert("SUCCESS! SENT TO YOUR EMAIL!");
+      // window.open(file.file);
+      return file;
+    } else {
+      alert("SOMETHING WENT WRONG");
+      const error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function downloadList() {
+  try {
+    let response = await fetch(
+      `http://localhost:3001/products/csv/exportToCSV`,
+      { method: "GET" }
+    );
+    const file = { file: `http://localhost:3001/products/csv/exportToCSV` };
+
+    if (response.ok) {
+      // alert("SUCCESS!");
+      window.open(file.file);
+    } else {
+      alert("SOMETHING WENT WRONG");
+      const error = response.json();
+      return error;
+    }
+  } catch (error) {
+    return error;
+  }
+}
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row, Card, Button, Spinner } from "react-bootstrap";
+import { getProducts } from "../api/productsApi";
+import Reviews from "./Reviews";
+const ProductList = (props) => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [productId, setProductId] = useState("");
+  const [modalShow, setModalShow] = useState(false);
+
+  useEffect(() => {
+    const callMeNow = async () => {
+      await fetchProducts();
+    };
+
+    callMeNow();
+  }, []);
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    const allProducts = await getProducts();
+    setProducts(allProducts);
+    setLoading(false);
+  };
+
+  const sendProduct = (product) => {
+    props.addToBasket(product);
+
+    console.log(product);
+  };
+  return (
+    <div className="product-list mt-4">
+      <Reviews
+        productId={productId}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      <Container>
+        {loading ? (
+          <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        ) : (
+          <Row>
+            {products.map((product) => {
+              return (
+                <Col md={3} key={product._id}>
+                  <Card style={{ width: "12rem" }}>
+                    <Card.Img
+                      style={{
+                        width: "100%",
+                        height: "10rem",
+                      }}
+                      variant="top"
+                      src={`http://localhost:3001/${product._id}.jpg`}
+                    />
+                    <Card.Body>
+                      <Card.Title>{product.name}</Card.Title>
+                      <Card.Text>
+                        {product.description} <br />
+                        <strong>${product.price}</strong>
+                      </Card.Text>
+                      <Button
+                        onClick={() => {
+                          setProductId(product._id);
+                          setModalShow(true);
+                        }}
+                        variant="primary"
+                      >
+                        Reviews
+                      </Button>
+                      <Button
+                        className="ml-3"
+                        onClick={() => sendProduct(product)}
+                      >
+                        Buy
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default ProductList;
